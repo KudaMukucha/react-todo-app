7370c6528adba27ff26be215527f113ea7139a7e
KudaMukucha <kudamukucha80@gmail.com>
2023-11-13 15:20:33 +0200

    initial commit

---
+++ src/components/TodoItem.jsx
@@ -0,0 +1,83 @@
+import styled,{keyframes} from 'styled-components'
+import { FaTrash } from 'react-icons/fa'
+import { useState } from 'react'
+
+const shrink = keyframes`
+0%{
+    transform:scale(1);
+}
+50%{
+    transform:scale(0.9)
+}
+100%{
+    transform:scale(1)
+}
+`
+
+const TodoItemStyle = styled.li`
+    list-style-type:none;
+    span{
+        display:flex;
+        align-items:center;
+        h4{
+            margin-right:1rem;
+            color:#3ad4b0;
+        }
+        h3{
+            text-decoration:${(props)=> props.checked && "line-through"}
+        }
+    }
+    color:${(props)=> (props.checked ? "gray" : "#fff")};
+    border:1px solid #3ad4b0;
+    border-radius:1px;
+    margin-bottom:1rem;
+    display:flex;
+    justify-content:space-between;
+    transition:all .3s ease-in-out;
+    padding:0.35rem 1rem;
+    &:hover{
+        background:#3ad4b0;
+        h4,button{
+            color:#111
+        }
+    }
+    &.checked{
+        animation:${shrink} 0.3s ease-in-out;
+    }
+    button{
+        cursor:pointer;
+        background:transparent;
+        border:none;
+        font-size:1rem;
+        color:#3ad4b0;
+        transition:all .3s ease-in-out;
+        &:hover{
+            color:#d44a4a
+        }
+    }
+    `
+const TodoItem = ({nr,todo,deleteTodo}) => {
+    const [isChecked,setIsChecked] = useState({
+        completed:false,
+        button:false,
+
+    })
+    const handleCheckItem=()=>{
+        setIsChecked({completed:!isChecked.completed,button:!isChecked.button})
+    }
+  return (
+    <TodoItemStyle onClick={handleCheckItem} {...{checked:isChecked.completed}} className={isChecked.completed && 'checked'}>
+        <span>
+            <h4>{nr + 1}</h4>
+            <h3>{todo}</h3>
+        </span>
+        {
+            isChecked.button &&(
+                <button onClick={deleteTodo}><FaTrash/></button>
+            )
+        }
+    </TodoItemStyle>
+  )
+}
+
+export default TodoItem
---
+++ src/components/TodoList.jsx
@@ -0,0 +1,95 @@
+import { useState } from 'react';
+import styled from 'styled-components';
+import TodoItem from './TodoItem';
+
+const TodoListStyle = styled.div`
+    margin:0 auto;
+    padding:1rem;
+    width:500px;
+    background-color:#141414;
+    border-radius:5px;
+    box-shadow:0 0 15px rgba(0,0,0,.2);
+    h1{
+        color:#ddd;
+        text-align:center;
+        margin-bottom:2rem;
+    }
+    form{
+        display:flex;
+        justify-content:center;
+        align-items:center;
+        margin-bottom:1.5rem;
+        input{
+            padding:0.5rem;
+            background:#ddd;
+            border:none;
+            border-radius:5px 0 0 5px;
+            width:80%;
+            transition:all .3s ease-in-out;
+            &:hover{
+                background:#fff;
+            }
+            &:active{
+                background:#fff;
+                outline:none;
+            }
+            &:focus{
+                background:#fff;
+                outline:none;
+            }
+        }
+        button{
+            border:none;
+            padding:0.5rem 1rem;
+            border-radius:0px 5px 5px 0px;
+            background:#3ad4b0;
+            color:#141414;
+            font-weight:500;
+            cursor:pointer;
+        }
+    }
+`
+const TodoList = () => {
+    const [todos,setTodos]=useState([])
+    const [inputValue,setInputValue]=useState("")
+
+    document.body.style = 'background:#3ad4b0'
+    const handleSubmit=(e)=>{
+        e.preventDefault()
+        //console.log('submit')
+
+        //validation
+        inputValue === "" && alert('Please add item!')
+        setTodos([...todos,inputValue.toUpperCase()])
+        setInputValue("");
+    }
+
+    const handleInputChange=(e)=>{
+        setInputValue(e.target.value)
+    }
+
+    const handleDeleteTodo=(index)=>{
+        if(!window.confirm("Are you sure you want to delete this item?")){
+            return
+        }
+        const newTodos = [...todos];
+        newTodos.splice(index,1);
+        setTodos(newTodos)
+    }
+  return (
+    <TodoListStyle>
+        <h1>Todo List</h1>
+        <form onSubmit={handleSubmit}>
+            <input placeholder="Add todo" onChange={handleInputChange}/>
+            <button type="submit">Add</button>
+        </form>
+        <ul>
+          {
+            todos.map((todo,index)=>(<TodoItem key={index} todo={todo} nr={index} deleteTodo ={handleDeleteTodo}/>))
+          }       
+        </ul>
+    </TodoListStyle>
+  )
+}
+
+export default TodoList
